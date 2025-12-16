00852f159023b6ecbb8ae3951de67029924f7399
LeusAlex <sanya.leus@yandex.ru>
2025-12-16 18:11:36 +0300

    Шаг 4. Обновил файлы.

--- src/main/java/ru/leus/data_service/StudentController.java
+++ src/main/java/ru/leus/data_service/StudentController.java
@@ -1,5 +1,37 @@
 package ru.leus.data_service;
 
-public class StudentController {
+import ru.leus.data.model.StudentDataCreateRequest;
+import ru.leus.data.model.StudentDataResponse;
+import ru.leus.data.api.StudentDataApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+import ru.leus.data_service.Student;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentController implements StudentDataApi {
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public ResponseEntity<StudentDataResponse> createStudentDataInData(StudentDataCreateRequest request) {
+        Student student = new Student();
+        student.setName(request.getFullName());
+        student.setPassport(request.getPassport());
+        studentRepository.save(student);
+        StudentDataResponse response = new StudentDataResponse();
+        response.setId(student.getId());
+        response.setFullName(student.getName());
+        response.setPassport(student.getPassport());
+
+        return ResponseEntity.status(200).body(response);
+    }
+
+  /*@Override
+  public ResponseEntity<StudentDataResponse> getStudentDataByIdFromData(Long id) {
+
+    return ResponseEntity.status(200).body(response);
+  }*/
 }
--- src/main/java/ru/leus/data_service/StudentRepository.java
+++ src/main/java/ru/leus/data_service/StudentRepository.java
@@ -1,2 +1,7 @@
+package ru.leus.data_service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
